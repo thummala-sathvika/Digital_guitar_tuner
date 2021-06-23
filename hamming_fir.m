2eb16a4e5787835047c041a8b86b0911a35436d2
thummala-sathvika <68094844+thummala-sathvika@users.noreply.github.com>
2021-06-23 22:25:25 +0530

    Update hamming_fir.m

--- hamming_fir.m
+++ hamming_fir.m
@@ -1,48 +1,140 @@
-clc;
 clear all;
+clc;
 close all;
-rp=input('enter the passband ripple');
-rs=input('enter the stopband ripple');
-fp=input('enter the passband freq');
-fs=input('enter the stopband freq');
-f=input('enter the sampling freq');
-wp=2*fp/f;
-ws=2*fs/f;
+range=30;
+x=isempty (input ('Press enter to start program or press zero to
+quit.'));
+p=input ('Which string are you adjusting? [1=E 2=B 3=G 4=D 5=A
+6=Emin]: ');
+fc=0.1;
+Fs=500;
+samples = [1,2*Fs];
+[Z,Fs] = audioread('g.wav',samples)
+m=length(Z);
+n=0:1:m-1;
+l=n-(m-1)/2;
+plot(Z); %fig 1
+title('Input signal')
+xlabel('n')
+ylabel('Z')
+figure;
+[h,w]=fft(Z);
+plot(w/pi,abs(h));grid %fig 2
+title('Frequency response of input function')
+xlabel('Frequency')
+ylabel('Amplitude')
+rp=0.891;
+rs=0.0032;
+fp=75;
+fs=1320;
+wp=2*fp/Fs;
+ws=2*fs/Fs;
 num=-20*log10(sqrt(rp*rs))-13;
-dem=14.6*(fs-fp)/f; n=ceil(num/dem);
+dem=14.6*(fs-fp)/Fs;
+n=ceil(num/dem);
 n1=n+1;
 if (rem(n,2)~=0)
-n1=n; n=n-1;
+n1=n;
+n=n-1;
 end
 y=hamming(n1);
-% low-pass filter
-b=fir1(n,wp,y);
-[h,o]=freqz(b,1,256);
-m=20*log10(abs(h));
-subplot(2,2,1);
-plot(o/pi,m);ylabel('Gain in dB --.');
-xlabel('(a) Normalised frequency --.');
-% high-pass filter
-b=fir1(n,wp,'high',y);
-[h,o]=freqz(b,1,256);
-m=20*log10(abs(h));
-subplot(2,2,2);
-plot(o/pi,m);
-ylabel('Gain in dB --.');
-xlabel('(b) Normalised frequency --.');
-% band pass filter
 wn=[wp ws];
 b=fir1(n,wn,y);
 [h,o]=freqz(b,1,256);
 m=20*log10(abs(h));
-subplot(2,2,3);
-plot(o/pi,m);ylabel('Gain in dB -->');
-xlabel('(c) Normalised frequency -->');
-% band stop filter
-b=fir1(n,wn,'stop',y);
-[h,o]=freqz(b,1,256);
-m=20*log10(abs(h));
-subplot(2,2,4);
-plot(o/pi,m);
-ylabel('Gain in dB -->');
-xlabel('(d) Normalised frequency -->');
+figure;
+plot(o/pi,m);ylabel('Gain in dB');
+xlabel('Normalised frequency ');
+grid
+title('Hamming Window')
+figure;
+t=filter(b,1,Z);
+plot(t); %fig 4
+title('Filtered audio signal using hamming window')
+xlabel('freq')
+ylabel('Amplitude')
+figure;
+y1=downsample(t,2);
+plot(y1);
+title('Downsampling by factor 2')
+xlabel('Freq')
+ylabel('Amplitude')
+y4 = padarray(y1,[500,0],0,'post')
+figure;
+y2=downsample(t,3);
+plot(y2);
+title('Downsampling by factor 3')
+xlabel('Freq')
+ylabel('Amplitude')
+y5 = padarray(y2,[666,0],0,'post')
+figure;
+y3=downsample(t,4);
+plot(y3);
+title('Downsampling by factor 4')
+xlabel('Freq')
+ylabel('Amplitude')
+y6 = padarray(y3,[750,0],0,'post')
+y7=y4+y5+y6+t;
+figure;
+plot(y7);
+title('Harmonic addition spectrum')
+xlabel('Freq')
+ylabel('Amplitude')
+[m,index]=max(y7)
+max=max(index)
+if p==1
+ fprintf('The input fundamental frequency is : %d\n', max');
+if ((330+range)>(max)&& (330-range)<(max)),
+display ('Perfect Intonation!!!');
+elseif (330-range)>(max),
+display ('Input frequency should be increased');
+elseif (330+range)<(max),
+display ('Input frequency should be decreased');
+end
+elseif p==2
+fprintf('The input fundamental frequency is : %d\n', max');
+if ((246.9+range)>(max)&& (246.9-range)<(max)),
+display ('Perfect Intonation!!!');
+elseif (246.9-range)>(max),
+display ('Input frequency should be increased');
+elseif (246.9+range)<(max),
+display ('Input frequency should be decreased');
+end
+elseif p==3
+fprintf('The input fundamental frequency is : %d\n', max');
+if ((196+range)>(max)&& (196-range)<(max)),
+display ('Perfect Intonation!!!');
+elseif (196-range)>(max),
+display ('Input frequency should be increased');
+elseif (196+range)<(max),
+display ('Input frequency should be decreased');
+end
+ elseif p==4
+fprintf('The input fundamental frequency is : %d\n', max');
+if ((146.83+range)>(max)&& (146.83-range)<(max)),
+display ('Perfect Intonation!!!');
+elseif (146.83-range)>(max),
+display ('Input frequency should4 be increased');
+elseif (146.83+range)<(max),
+display ('Input frequency should be decreased');
+end
+ elseif p==5
+fprintf('The input fundamental frequency is : %d\n', max');
+if ((110+range)>(max)&& (110-range)<(max)),
+display ('Perfect Intonation!!!');
+elseif (110-range)>(max),
+display ('Input frequency should be increased');
+elseif (110+range)<(max),
+display ('Input frequency should be decreased');
+end
+ elseif p==6
+fprintf('The input fundamental frequency is : %d\n', max');
+if ((82.4+range)>(max)&& (82.4-range)<(max)),
+display ('Perfect Intonation!!!');
+elseif (82.4-range)>(max),
+display ('Input frequency should be increased');
+elseif (82.4+range)<(max),
+display ('Input frequency should be decreased');
+end
+end
+display('Tune the guitar accordingly')
